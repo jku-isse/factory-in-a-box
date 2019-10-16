4809eecffba8d2e483f19f05dae90b6a24027978
Max Spatzenegger <max.spatzenegger@gmail.com>
2019-10-16 12:32:47 +0200

    merging everything locally

--- MES/FIAB/src/main/java/fiab/mes/eventbus/InterMachineEventBus.java
+++ MES/FIAB/src/main/java/fiab/mes/eventbus/InterMachineEventBus.java
@@ -36,44 +36,4 @@ public class InterMachineEventBus extends ScanningEventBus<MachineEvent, ActorRe
 	}
 
 
-=======
-package fiab.mes.eventbus;
-
-import akka.actor.ActorRef;
-import akka.event.japi.ScanningEventBus;
-import fiab.mes.general.TimedEvent;
-import fiab.mes.machine.msg.MachineEvent;
-import fiab.mes.order.msg.OrderEvent;
-
-public class InterMachineEventBus extends ScanningEventBus<MachineEvent, ActorRef, SubscriptionClassifier> {
-
-	
-	@Override
-	public void publish(MachineEvent event, ActorRef subscriber) {
-		subscriber.tell(event, ActorRef.noSender());
-	}
-
-	@Override
-	public int compareSubscribers(ActorRef a, ActorRef b) {
-		return a.compareTo(b);
-	}
-
-	@Override
-	public int compareClassifiers(SubscriptionClassifier a, SubscriptionClassifier b) {
-		return a.topic.compareTo(b.topic);
-		
-	}
-	
-	@Override
-	public boolean matches(SubscriptionClassifier classifier, MachineEvent event) {
-		if (classifier.eventSource.equals(event.getMachineId()))
-			return false; // we dont notify sender of event
-		if (classifier.topic.equals("*"))
-			return true;
-		else 
-			return classifier.topic.equals(event.getMachineId());
-	}
-
-
->>>>>>> 97b3554d4547806a51459ec861ef97dc386178a9
 }
