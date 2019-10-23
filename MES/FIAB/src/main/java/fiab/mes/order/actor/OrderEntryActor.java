6aa3f03c1033bd5031318e3a51d8744cb1f77eb0
stefanb <steff396@gmail.com>
2019-10-23 12:10:43 +0200

    MinimalTestServer functionality moved into a test class: OrderEmittingTestServer

--- MES/FIAB/src/main/java/fiab/mes/order/actor/OrderEntryActor.java
+++ MES/FIAB/src/main/java/fiab/mes/order/actor/OrderEntryActor.java
@@ -10,6 +10,9 @@ import akka.actor.ActorRef;
 import akka.actor.ActorSelection;
 import akka.actor.Props;
 import fiab.mes.eventbus.OrderEventBusWrapperActor;
+import fiab.mes.eventbus.SubscribeMessage;
+import fiab.mes.eventbus.SubscriptionClassifier;
+import fiab.mes.order.msg.OrderEvent;
 import fiab.mes.order.msg.RegisterProcessRequest;
 import fiab.mes.planer.actor.OrderPlanningActor;
 import fiab.mes.restendpoint.requests.OrderHistoryRequest;
@@ -23,8 +26,8 @@ public class OrderEntryActor extends AbstractActor{
 	private LoggingAdapter log = Logging.getLogger(getContext().getSystem(), this);
 	
 	private final AtomicInteger orderId = new AtomicInteger(0);
-	private HashMap<String, ActorRef> orderActors = new HashMap<>();	
-	private ActorSelection eventBusByRef;
+	private HashMap<String, ActorRef> orderActors = new HashMap<>();
+	private HashMap<String, OrderEvent> latestChange = new HashMap<>();	private ActorSelection eventBusByRef;
 	private ActorSelection orderPlannerByRef;
 	
 	static public Props props() {
@@ -34,13 +37,14 @@ public class OrderEntryActor extends AbstractActor{
 	public OrderEntryActor() {
 		eventBusByRef = context().actorSelection("/user/"+OrderEventBusWrapperActor.WRAPPER_ACTOR_LOOKUP_NAME);	
 		orderPlannerByRef = context().actorSelection("/user/"+OrderPlanningActor.WELLKNOWN_LOOKUP_NAME);	
+		eventBusByRef.tell(new SubscribeMessage(getSelf(), new SubscriptionClassifier(self().path().name(), "*")), getSelf());
 	}
 	
 	@Override
 	public Receive createReceive() {
 		 return receiveBuilder()
 			        .matchEquals("GetAllOrders", p -> {
-			        	sender().tell(orderActors.keySet(), getSelf());
+			        	sender().tell(latestChange.values(), getSelf());
 			        	// need to figure out how to collect order information from all order actors, perhaps a separate actors is needed for that
 			        	})
 			        .match(RegisterProcessRequest.class, doc -> {			        	
@@ -69,6 +73,9 @@ public class OrderEntryActor extends AbstractActor{
 			        		log.info("OrderHistoryRequest received for nonexisting order: "+req.getOrderId());
 			        		sender().tell(Optional.empty(), getSelf());
 			        	}
+			        })
+			        .match(OrderEvent.class, e -> {
+			        	this.latestChange.put(e.getOrderId(), e);
 			        })
 			        .matchAny(o -> log.info("OrderEntryActor received Invalid message type: "+o.getClass().getSimpleName()))
 			        .build();
--- MES/FIAB/src/test/java/fiab/mes/frontend/OrderEmittingTestServer.java
+++ MES/FIAB/src/test/java/fiab/mes/frontend/OrderEmittingTestServer.java
@@ -1,17 +1,16 @@
 package fiab.mes.frontend;
 
-import static akka.pattern.PatternsCS.ask;
-
 import java.io.IOException;
 import java.util.concurrent.CompletionStage;
 import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.TimeUnit;
 
 import org.junit.AfterClass;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import ActorCoreModel.Actor;
+import ProcessCore.AbstractCapability;
 import akka.NotUsed;
 import akka.actor.ActorRef;
 import akka.actor.ActorSelection;
@@ -24,40 +23,38 @@ import akka.http.javadsl.model.HttpResponse;
 import akka.stream.ActorMaterializer;
 import akka.stream.javadsl.Flow;
 import akka.testkit.javadsl.TestKit;
-import akka.util.Timeout;
 import fiab.mes.DefaultShopfloorInfrastructure;
 import fiab.mes.eventbus.InterMachineEventBusWrapperActor;
 import fiab.mes.eventbus.OrderEventBusWrapperActor;
+import fiab.mes.mockactors.MockMachineActor;
+import fiab.mes.mockactors.TestMockMachineActor;
+import fiab.mes.order.OrderProcess;
 import fiab.mes.order.actor.OrderEntryActor;
 import fiab.mes.order.msg.RegisterProcessRequest;
 import fiab.mes.planer.actor.OrderPlanningActor;
 import fiab.mes.restendpoint.ActorRestEndpoint;
-import scala.concurrent.duration.FiniteDuration;
 
 public class OrderEmittingTestServer {
 	
 	private static ActorSystem system;
 	private static String ROOT_SYSTEM = "routes";
-	private static ActorSelection machineEventBus;
-	private static ActorSelection orderEventBus;
-	private static ActorSelection orderPlanningActor;
+//	private static ActorSelection machineEventBus;
+//	private static ActorSelection orderEventBus;
+//	private static ActorSelection orderPlanningActor;
 	private static ActorRef orderEntryActor;
 	private static CompletionStage<ServerBinding> binding;
-	
+
+	private static OrderProcess process;
+
 	@BeforeAll
 	static void setUpBeforeClass() throws Exception {
-		// setup shopfloor
-		// setup machines
-		// setup processes
-		// setup order actors?
-		// add processes to orderplanning actor
 		system = ActorSystem.create(ROOT_SYSTEM);
 		final Http http = Http.get(system);
 	    final ActorMaterializer materializer = ActorMaterializer.create(system);
 	    DefaultShopfloorInfrastructure shopfloor = new DefaultShopfloorInfrastructure(system);
-	    orderEventBus = system.actorSelection("/user/"+OrderEventBusWrapperActor.WRAPPER_ACTOR_LOOKUP_NAME);
-	    machineEventBus = system.actorSelection("/user/"+InterMachineEventBusWrapperActor.WRAPPER_ACTOR_LOOKUP_NAME);
-	    orderPlanningActor = system.actorSelection("/user/"+OrderPlanningActor.WELLKNOWN_LOOKUP_NAME);
+//	    orderEventBus = system.actorSelection("/user/"+OrderEventBusWrapperActor.WRAPPER_ACTOR_LOOKUP_NAME);
+//	    machineEventBus = system.actorSelection("/user/"+InterMachineEventBusWrapperActor.WRAPPER_ACTOR_LOOKUP_NAME);
+//	    orderPlanningActor = system.actorSelection("/user/"+OrderPlanningActor.WELLKNOWN_LOOKUP_NAME);
 	    orderEntryActor = system.actorOf(OrderEntryActor.props());
 	    ActorRestEndpoint app = new ActorRestEndpoint(system, orderEntryActor);
 	
@@ -82,36 +79,46 @@ public class OrderEmittingTestServer {
 	    system = null;
 	}
 	
-	@SuppressWarnings("deprecation")
 	@Test
 	void testFrontendResponsesByEmittingOrders() {
 		try {
 			new TestKit(system) { 
 				{ 
 					System.out.println("testFrontendResponsesByEmittingOrders()");
+					
+					ActorRef red1 = getMachineMockActor(1, "Red");
+					ActorRef blue2 = getMachineMockActor(2, "Blue");
+					ActorRef green3 = getMachineMockActor(3, "Green");
+					ActorRef yellow4 = getMachineMockActor(4, "Yellow");
+					
+					process = new OrderProcess(TestMockMachineActor.getSequentialProcess());
+					
 				    CountDownLatch semaphore = new CountDownLatch(10);
-				    final Timeout timeout = Timeout.durationToTimeout(FiniteDuration.apply(5, TimeUnit.SECONDS));
 				    while(semaphore.getCount() > 0) {
-				    	System.out.println("create new order:");
-				    	ask(orderEntryActor, getRegisterProcessRequest(String.valueOf(semaphore.getCount()), system), timeout);
+				    	String id = String.valueOf(semaphore.getCount());
+				    	System.out.println("Inserting Order"+id);
+				    	
+				    	RegisterProcessRequest req = new RegisterProcessRequest(id, id, process, getRef());
+				    	orderEntryActor.tell(req, getRef());
+				    	
 				    	semaphore.countDown();
 				    	Thread.sleep(3000);
 				    }
+				    System.out.println("Finished with emitting orders. Press ENTER to end test!");
 				    System.in.read();
 				    System.out.println("Test completed");
 				}	
 			};
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
+		} catch (InterruptedException | IOException e) {
 			e.printStackTrace();
 		}
 	}
 	
-	private RegisterProcessRequest getRegisterProcessRequest(String id, ActorSystem system) {
-		setupSteps();
+	private static ActorRef getMachineMockActor(int id, String color) {
+		ActorSelection eventBusByRef = system.actorSelection("/user/"+InterMachineEventBusWrapperActor.WRAPPER_ACTOR_LOOKUP_NAME);
+		Actor modelActor = TestMockMachineActor.getDefaultMachineActor(id);
+		AbstractCapability cap = TestMockMachineActor.composeInOne(TestMockMachineActor.getPlottingCapability(), TestMockMachineActor.getColorCapability(color));
+		return system.actorOf(MockMachineActor.props(eventBusByRef, cap, modelActor));
 	}
 
 }
