fcb76333e43cd6b281a6a5cfc5e2e5672ff7bd24
Max Spatzenegger <max.spatzenegger@gmail.com>
2019-12-19 11:31:23 +0100

    Imports

---
+++ OPC-UA/HardwareMockedMachines/src/fiab/mes/machine/msg/MachineEvent.java
@@ -0,0 +1,62 @@
+package fiab.mes.machine.msg;
+
+import java.time.ZonedDateTime;
+
+import fiab.mes.general.TimedEvent;
+
+public class MachineEvent extends TimedEvent {
+	
+	private String message;
+	protected String machineId;
+	protected MachineEventType eventType;
+	
+	public MachineEvent(String machineId, MachineEventType eventType) {
+		super();
+		this.machineId = machineId;
+		this.eventType = eventType;
+		this.message = "";
+	}
+	
+	public MachineEvent(String machineId, MachineEventType eventType, String message) {
+		super();
+		this.machineId = machineId;
+		this.eventType = eventType;
+		this.message = message;
+	}
+	
+	public MachineEvent(String machineId, MachineEventType eventType, String message, ZonedDateTime timestamp) {
+		super(timestamp);
+		this.machineId = machineId;
+		this.eventType = eventType;
+		this.message = message;
+	}
+	
+	public MachineEvent(String machineId, MachineEventType eventType, ZonedDateTime timestamp) {
+		super(timestamp);
+		this.machineId = machineId;
+		this.eventType = eventType;
+		this.message = "";
+	}
+	
+	public MachineEventType getEventType() {
+		return eventType;
+	}
+	
+	
+	public String getMachineId() {
+		return machineId;
+	}
+	
+	public String getMessage() {
+		return message;
+	}
+	
+	public MachineEvent getCloneWithoutDetails() {
+		return new MachineEvent(machineId, eventType, message, getTimestamp());
+	}
+	
+	public static enum MachineEventType {
+		CONNECTED, DISCONNECTED, UPDATED
+	}
+
+}
---
+++ OPC-UA/HardwareMockedMachines/src/fiab/mes/mockactors/iostation/MockOutputStationServerHandshakeActor.java
@@ -0,0 +1,76 @@
+package fiab.mes.mockactors.iostation;
+
+import java.time.Duration;
+
+import com.google.common.collect.ImmutableSet;
+
+import akka.actor.ActorRef;
+import akka.actor.Props;
+import fiab.mes.mockactors.MockServerHandshakeActor;
+import fiab.mes.transport.handshake.HandshakeProtocol.ServerSide;
+
+public class MockOutputStationServerHandshakeActor extends MockServerHandshakeActor{
+
+	static public Props props() {	    
+		return Props.create(MockOutputStationServerHandshakeActor.class, () -> new MockOutputStationServerHandshakeActor());
+	}
+	
+	public MockOutputStationServerHandshakeActor() {
+		super(null, true);
+	}
+	
+	@Override
+	protected void publishNewState(ServerSide newState) {
+		currentState = newState;
+		ImmutableSet.copyOf(subscribers).stream().forEach(sub -> sub.tell(newState, getSelf()));
+	}
+
+	// we accept the usual request but we implement an auto reset, except for an initial reset to become active
+	// and awaiting for being empty in the reset method, i.e., we wont ever reach idleFull
+	
+	@Override
+	protected void reset() {
+		publishNewState(ServerSide.Resetting);
+		context().system()
+    	.scheduler()
+    	.scheduleOnce(Duration.ofMillis(1000), 
+    			 new Runnable() {
+            @Override
+            public void run() {
+            	if (!isLoaded) {
+            		publishNewState(ServerSide.IdleEmpty);
+            	} else {
+            		// stay in resetting
+            	}
+            }
+          }, context().system().dispatcher());
+	}
+	
+	@Override
+	protected void updateLoadState(boolean isLoaded) {
+		if (this.isLoaded != isLoaded) {
+			this.isLoaded = isLoaded;
+			if (currentState.equals(ServerSide.Resetting)) {
+				reset(); // we reset again to reach IdleLoaded
+			} else if (currentState != ServerSide.Stopped) {
+				stopAndAutoReset();
+			}
+		}
+	}
+	
+	private void stopAndAutoReset() {
+		publishNewState(ServerSide.Stopping);
+		clientSide = null;
+		context().system()
+    	.scheduler()
+    	.scheduleOnce(Duration.ofMillis(1000), 
+    			 new Runnable() {
+            @Override
+            public void run() {
+            	publishNewState(ServerSide.Stopped);
+            	reset();
+            }
+          }, context().system().dispatcher());
+	}
+	
+}
