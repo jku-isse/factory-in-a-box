4cfccf886825bd71d225a865ec421e1c00059272
Christoph Mayr-Dorn <c.mayr-dorn@jku.at>
2020-02-20 15:09:56 +0100

    started introducing mock turntable and conveyor actors to turntable actor, not done yet

---
+++ MES/FIAB/src/test/java/stateMachines/turning/TurningStates.java
@@ -0,0 +1,16 @@
+package stateMachines.turning;
+
+public enum TurningStates {
+    IDLE(0), STARTING(1), EXECUTING(2), COMPLETING(3), RESETTING(4), COMPLETE(5),
+    STOPPING(6), STOPPED(7);
+
+    private int value;
+
+    TurningStates(int value){
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+}
