a317f9f538e058d164b8d02bdc412f585fa7c875
Christoph Mayr-Dorn <c.mayr-dorn@jku.at>
2020-05-07 14:48:45 +0200

    removed direct project imports and using jars in lib folders via maven instead

--- FiABopcuabase/src/fiab/opcua/CapabilityExposingUtils.java
+++ FiABopcuabase/src/fiab/opcua/CapabilityExposingUtils.java
@@ -7,37 +7,36 @@ import org.eclipse.milo.opcua.sdk.server.nodes.UaFolderNode;
 
 import fiab.core.capabilities.meta.OPCUACapabilitiesAndWiringInfoBrowsenames;
 import fiab.opcua.server.OPCUABase;
-import lombok.Value;
 
 public class CapabilityExposingUtils {
 
-	public static void setupTurntableCapabilities(OPCUABase opcuaBase, UaFolderNode ttNode, String path, CapabilityProvisioning cp) {
-			setupTurntableCapabilities(opcuaBase, ttNode, path, "", cp);
+	public static void setupCapabilities(OPCUABase opcuaBase, UaFolderNode ttNode, String path, CapabilityImplementationMetadata cp) {
+			setupCapabilities(opcuaBase, ttNode, path, "", cp);
 	}
 		
-	private static void setupTurntableCapabilities(OPCUABase opcuaBase, UaFolderNode ttNode, String path, String capabilityPostfix, CapabilityProvisioning cp) {
+	private static void setupCapabilities(OPCUABase opcuaBase, UaFolderNode ttNode, String path, String capabilityPostfix, CapabilityImplementationMetadata cp) {
 		// add capabilities 
 		String capString = OPCUACapabilitiesAndWiringInfoBrowsenames.CAPABILITY+capabilityPostfix;
 		UaFolderNode capabilitiesFolder = opcuaBase.generateFolder(ttNode, path, new String( OPCUACapabilitiesAndWiringInfoBrowsenames.CAPABILITIES));
 		path = path +"/"+OPCUACapabilitiesAndWiringInfoBrowsenames.CAPABILITIES;
 		UaFolderNode capability1 = opcuaBase.generateFolder(capabilitiesFolder, path,
 				"CAPABILITY",  OPCUACapabilitiesAndWiringInfoBrowsenames.CAPABILITY);
-		opcuaBase.generateStringVariableNode(capability1, path+"/"+capString,  OPCUACapabilitiesAndWiringInfoBrowsenames.TYPE, cp.getUri());
-		opcuaBase.generateStringVariableNode(capability1, path+"/"+capString,  OPCUACapabilitiesAndWiringInfoBrowsenames.ID, cp.getId());
+		opcuaBase.generateStringVariableNode(capability1, path+"/"+capString,  OPCUACapabilitiesAndWiringInfoBrowsenames.TYPE, cp.getCapabilityURI());
+		opcuaBase.generateStringVariableNode(capability1, path+"/"+capString,  OPCUACapabilitiesAndWiringInfoBrowsenames.ID, cp.getImplId());
 		opcuaBase.generateStringVariableNode(capability1, path+"/"+capString,  OPCUACapabilitiesAndWiringInfoBrowsenames.ROLE, 
 				cp.isProvided() ? OPCUACapabilitiesAndWiringInfoBrowsenames.ROLE_VALUE_PROVIDED : OPCUACapabilitiesAndWiringInfoBrowsenames.ROLE_VALUE_REQUIRED);
 	}
 
-	public static void setupTurntableCapabilities(OPCUABase opcuaBase, UaFolderNode ttNode, String path, List<CapabilityProvisioning> cps) {
+	public static void setupCapabilities(OPCUABase opcuaBase, UaFolderNode ttNode, String path, List<CapabilityImplementationMetadata> cps) {
 		IntStream.range(0, cps.size())
-		.forEach(i -> setupTurntableCapabilities(opcuaBase, ttNode, path, ""+(i+1), cps.get(i)) );
+		.forEach(i -> setupCapabilities(opcuaBase, ttNode, path, ""+(i+1), cps.get(i)) );
 	}
 	
 	
-	@Value public static class CapabilityProvisioning {
-		String uri;
-		String id;
-		boolean isProvided;
-	}
+//	@Value public static class CapabilityProvisioning {
+//		String uri;
+//		String id;
+//		boolean isProvided;
+//	}
 	
 }
