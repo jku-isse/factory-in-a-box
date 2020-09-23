ee3ef8bdcb10c10c49f97de1fc93403dfb6a0061
holzweber <holzweber@LAPTOP-V7KDVHKN.mshome.net>
2020-09-23 14:21:43 +0200

    Added AbstractTracingActor #59

---
+++ FIABtracing/src/fiab/tracing/actor/AbstractTracingActor.java
@@ -0,0 +1,22 @@
+package fiab.tracing.actor;
+
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+
+import akka.actor.AbstractActor;
+import fiab.tracing.config.Util;
+import fiab.tracing.factory.TracingFactory;
+
+public abstract class AbstractTracingActor extends AbstractActor {
+	private final TracingFactory tracingFactory;
+
+	public AbstractTracingActor() {
+		Injector injector = Guice.createInjector(Util.getConfig());
+		tracingFactory = injector.getInstance(TracingFactory.class);
+	}
+
+	public TracingFactory getTracingFactory() {
+		return tracingFactory;
+	}
+
+}
